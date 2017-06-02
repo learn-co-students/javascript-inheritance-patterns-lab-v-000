92d66d2cfa3fa691cc3bb5bb0ee581b653004ac7
John Pfingst <jpfingst@outlook.com>
2017-06-02 23:40:27 +0000

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function Circle(radius) {
 
 Circle.prototype = Object.create(Shape.prototype);
 Circle.prototype.constructor = Circle;
-Circle.prototype.area = function() { return (Math.PI * this.radius * this.radius) }
+Circle.prototype.area = function() { return (Math.PI * this.radius^2) }
 Circle.prototype.circumference = function() { return Math.PI * this.diameter }
 Circle.prototype.diameter = function() { return 2 * this.radius }
 
@@ -41,3 +41,42 @@ Polygon.prototype.perimeter = function() {
   }, 0 )
 }
 
+function Quadrilateral(l1, l2, l3, l4) {
+  var sides = [new Side(l1), new Side(l2), new Side(l3), new Side(l4)];
+  Polygon.call(this, sides);
+}
+
+Quadrilateral.prototype = Object.create(Polygon.prototype);
+Quadrilateral.prototype.constructor = Quadrilateral;
+
+function Triangle(l1, l2, l3) {
+  var sides = [new Side(l1), new Side(l2), new Side(l3)];
+  Polygon.call(this, sides);
+}
+
+Triangle.prototype = Object.create(Polygon.prototype);
+Triangle.prototype.constructor = Triangle;
+
+function Rectangle(width, height) {
+  Quadrilateral.call(this, width, height, width, height);
+  this.width = width;
+  this.height = height;
+}
+
+Rectangle.prototype = Object.create(Quadrilateral.prototype);
+Rectangle.prototype.constructor = Rectangle;
+Rectangle.prototype.area = function() { return this.height * this.width }
+
+function Square(length) {
+  Rectangle.call(this, length, length);
+}
+
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
+Rectangle.prototype.listProperties = function() {
+  for (var prop in this) {
+    if(this.hasOwnProperty(prop)) {
+      console.log("Rectangle." + prop + " = " + this[prop]);
+    }
+  }
+}
