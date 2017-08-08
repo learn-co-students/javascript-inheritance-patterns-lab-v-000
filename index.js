b0b4dc44700766bdd6e47de658188a5c45f1f862
machen2 <victoriamachen@gmail.com>
2017-08-08 20:17:57 +0000

    Done.

--- index.js
+++ index.js
@@ -25,21 +25,23 @@ Shape.prototype.move = function(x, y) {
 function Circle(radius) {
   Shape.call(this);
   this.radius = radius;
-  this.diameter = function() { return radius * 2}
 }
 Circle.prototype = Object.create(Shape.prototype);
 Circle.prototype.constructor = Circle;
+Circle.prototype.diameter = function() {
+  return (this.radius * 2);
+}
 Circle.prototype.area = function() {
-  return this.diameter * Math.PI;
+  return (this.radius * this.radius * Math.PI);
 }
 Circle.prototype.circumference = function() {
-  return this.radius * 2 * Math.PI;
+  return (this.radius * 2 * Math.PI);
 }
 
 //Define a Polygon object that inherits from Shape. It should be constructed with an array of Side objects that have a length property. Polygon should have a property called sides that
 //holds the array of Side objects. Implement a function called perimeter() that calculates the perimeter of any Polygon based on the lengths of the sides. Implement a function called
 //numberOfSides() that returns the number of sides.
-function Polygon(sides) {
+function Polygon(sides) { //array of side objects
   Shape.call(this);
   this.sides = sides //holds array of Side objects
 }
@@ -58,4 +60,50 @@ Polygon.prototype.numberOfSides = function() {
 
 function Side(length) {
   this.length = length;
+}
+
+//Define a Quadrilateral object that inherits from Polygon and is constructed with four integer arguments representing the side lengths.
+function Quadrilateral(sideOne, sideTwo, sideThree, sideFour) {
+  Polygon.call(this, [new Side(sideOne), new Side(sideTwo), new Side(sideThree), new Side(sideFour)]);
+}
+Quadrilateral.prototype = Object.create(Polygon.prototype);
+Quadrilateral.prototype.constructor = Quadrilateral;
+
+//Define a Triangle object that inherits from Polygon and is constructed with three integer arguments representing the side lengths.
+function Triangle(sideOne, sideTwo, sideThree) {
+  Polygon.call(this, [new Side(sideOne), new Side(sideTwo), new Side(sideThree)]);
+}
+Triangle.prototype = Object.create(Polygon.prototype);
+Triangle.prototype.constructor = Triangle;
+
+//Define a Rectangle object that inherits from Quadrilateral and is constructed with two integer arguments that set width and height properties. Implement an area() function to calculate the area.
+function Rectangle(width, height) {
+  Quadrilateral.call(this, width, height, width, height);
+  this.width = width;
+  this.height = height;
+}
+Rectangle.prototype = Object.create(Quadrilateral.prototype);
+Rectangle.prototype.constructor = Rectangle;
+Rectangle.prototype.area = function() {
+  return this.width * this.height;
+}
+
+//Define a Square object that inherits from Rectangle and is constructed with a single integer argument that sets a length property. If everything is wired up right in the prototype chain,
+//Square should have access to area(), perimeter(), numberOfSides(), addToPlane(), position, move(), width, height and so on.
+function Square(sideLength) {
+  Rectangle.call(this, sideLength, sideLength);
+  this.length = sideLength;
+}
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
+//Define and implement a function for Square called listProperties() that returns a string containing only the properties that belong to Square.
+//It should not list the constructor, area, perimeter, and other things inherited from the prototype chain.
+Square.prototype.listProperties = function() {
+  var squareProperties = "";
+  for (var prop in this) {
+    if (this.hasOwnProperty(prop)) {
+      squareProperties +=   "this." + prop + " = " + this[prop] + "\n";
+    }
+  }
+  return squareProperties;
 }
