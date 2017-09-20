47811564c8c4adbe360e43c9c039caa40b6f7b55
github username <github email address>
2017-09-20 15:07:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,86 +1,94 @@
 function Point(x, y) {
-    this.x = x;
-    this.y = y;
+	this.x = x;
+	this.y = y;
 }
 
 Point.prototype.toString = function() {
-    return this.x + ", " + this.y
-}
-function Side(length) {
-    this.length = length;
+	return this.x + ", " + this.y
 }
 
 function Shape() {}
 
-Shape.prototype.addToPlane = function(x, y) {
-    this.position = new Point(x, y);
+Shape.prototype.addToPlane = function(x,y) {
+	this.position = new Point(x, y)
 }
 
 Shape.prototype.move = function(x, y) {
-    this.position = new Point(x, y)
+	this.position = new Point(x, y)
 }
 
 function Circle(radius) {
-    Shape.call(this);
-    this.radius = radius;
+	this.radius = radius
 }
 
-Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype = Object.create(Shape.prototype)
+
+Circle.prototype.constructor = Circle
+
 Circle.prototype.diameter = function() {
-    return this.radius * 2
+	return this.radius * 2
 }
 
 Circle.prototype.area = function() {
-    return Math.PI * this.radius ** 2
+	return Math.PI * (2 * this.radius)
 }
 
 Circle.prototype.circumference = function() {
-    return 2 * Math.PI * this.radius
+	return 2 * (Math.PI * this.radius)
 }
+
+function Side(length) {
+	this.length = length
+}
+
 function Polygon(sidesArray) {
-    Shape.call(this)
-    this.sides = sidesArray
+	this.sides = sidesArray
 }
 
 Polygon.prototype = Object.create(Shape.prototype)
 
 Polygon.prototype.perimeter = function() {
-    var total = 0;
-    debugger;
-    this.sides.forEach(function(element) {
-        total += element.length;
-    }, this);
-    return total;
+	var total = 0
+	for (var i=0; i<this.sides.length; i++) {
+		total += this.sides[i].length
+	}
+	return total
 }
 
 Polygon.prototype.numberOfSides = function() {
-    return this.sides.length;
-} 
+	return this.sides.length
+}
 
 function Quadrilateral(side1, side2, side3, side4) {
-    var sidesArray = [new Side(side1), new Side(side2), new Side(side3), new Side(side4)]
-    Polygon.call(this, sidesArray)
+	Polygon.call(this, [new Side(side1), new Side(side2), new Side(side3), new Side(side4)])
 }
 
 Quadrilateral.prototype = Object.create(Polygon.prototype)
 
+function Triangle(side1, side2, side3) {
+	Polygon.call(this, [new Side(side1), new Side(side2), new Side(side3)])
+}
+
+Triangle.prototype = Object.create(Polygon.prototype)
+
 function Rectangle(width, height) {
-    Quadrilateral.call(this, width, width, height, height);
-    this.width = width;
-    this.height = height;
+	this.width = width
+	this.height = height
+	Quadrilateral.call(this, width, width, height, height)
 }
 
 Rectangle.prototype = Object.create(Quadrilateral.prototype)
 
 Rectangle.prototype.area = function() {
-    return this.width * this.height;
+	return this.width * this.height
 }
 
 function Square(length) {
-    Rectangle.call(this, length, length, length, length)
+	this.length = length
+	Rectangle.call(this, length, length)
 }
 
-Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype = Object.create(Rectangle.prototype)
 
 Square.prototype.listProperties = function() {
     for (var prop in this) {
@@ -90,8 +98,5 @@ Square.prototype.listProperties = function() {
     }
 }
 
-function Triangle(side1, side2, side3) {
-    Polygon.call(this, [new Side(side1), new Side(side2), new Side(side3)])
-}
 
-Triangle.prototype = Object.create(Polygon.prototype)
+
